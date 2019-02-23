71c0c6bdd09956a40aa0546267ff3d9cac2b2ca1
haoyan.zhang <haoyan.zhang@aliyun.com>
2019-02-23 22:44:18 +0800

    1

--- src/main/java/com/itaj/matrixMultiplication/MergeMatrixMultiplication.java
+++ src/main/java/com/itaj/matrixMultiplication/MergeMatrixMultiplication.java
@@ -10,4 +10,13 @@ public class MergeMatrixMultiplication implements MatrixMultiplication {
 
         return new int[0][];
     }
+
+    private  void mergeMatrixMultiplication(int square, int[][] a, int ax, int ay,  int[][] b, int bx, int by, int[][] result , int rx, int ry, int[][] additionSpace) {
+        int mid = (square + 1) >> 1;
+
+    }
+
+    private int get(int square, int[][] data, int x, int y, int tx, int ty) {
+
+    }
 }
