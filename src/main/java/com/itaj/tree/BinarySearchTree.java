389f8c55fe2e06da4877f1a3ae9ff5dc0c10df00
haoyan.zhang <haoyan.zhang@qunar.com>
2019-11-24 11:39:15 +0800

    'a'

--- src/main/java/com/itaj/tree/BinarySearchTree.java
+++ src/main/java/com/itaj/tree/BinarySearchTree.java
@@ -1,43 +1,69 @@
 package com.itaj.tree;
 
-public class BinarySearchTree {
+public class BinarySearchTree<T extends Comparable<T>> {
 
-    private TreeNode root;
+    private TreeNode<T> root;
 
     public BinarySearchTree() {
         root = null;
     }
 
+    private TreeNode find(T value) {
+        return find(root, value);
+    }
 
-    private static class TreeNode {
+    private TreeNode find(TreeNode<T> node, T value) {
+        if (node == null || node.getValue().equals(value)) {
+            return node;
+        }
+        if (node.getValue().compareTo(value) > 0) {
+            return find(node.getLeftChild(), value);
+        }
+        return find(node.getRightChild(), value);
+    }
 
-        public TreeNode(TreeNode leftChild, TreeNode rightChild) {
+
+    private static class TreeNode<T extends Comparable<T>> {
+
+        private T value;
+
+        public TreeNode(T value, TreeNode<T> leftChild, TreeNode<T> rightChild) {
+            this.value = value;
             this.leftChild = leftChild;
             this.rightChild = rightChild;
         }
 
-        public TreeNode() {
+        public TreeNode(T value) {
+            this.value = value;
             leftChild = null;
             rightChild = null;
         }
 
-        private TreeNode leftChild;
+        private TreeNode<T> leftChild;
 
-        private TreeNode rightChild;
+        private TreeNode<T> rightChild;
 
-        public TreeNode getLeftChild() {
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public TreeNode<T> getLeftChild() {
             return leftChild;
         }
 
-        public void setLeftChild(TreeNode leftChild) {
+        public void setLeftChild(TreeNode<T> leftChild) {
             this.leftChild = leftChild;
         }
 
-        public TreeNode getRightChild() {
+        public TreeNode<T> getRightChild() {
             return rightChild;
         }
 
-        public void setRightChild(TreeNode rightChild) {
+        public void setRightChild(TreeNode<T> rightChild) {
             this.rightChild = rightChild;
         }
     }
